19ba0fb5685551b8b6174a61f5037fc30f5a4297
Your Name <tasha.urbancic@gmail.com>
2017-09-07 19:38:33 +0000

    added examples to map.js

--- map.js
+++ map.js
@@ -7,3 +7,23 @@ function map(arr, cb) {
   }
   return output;
 }
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+var output1 = map(words, function(word) {
+  return word.length;
+});
+
+var output2 = map(words, function(word) {
+  return word.toUpperCase();
+});
+
+var output3 = map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+console.log(words);
+console.log(output1);
+console.log(output2);
+console.log(output3);
+
